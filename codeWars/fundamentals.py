11e22b13ae27d52f1bfe101d4ac1d2c29b95cc72
MReeds <mattlreeds@gmail.com>
2020-08-12 19:35:59 -0500

    another algorithm

--- codeWars/fundamentals.py
+++ codeWars/fundamentals.py
@@ -21,3 +21,25 @@ def title_case(title, minor_words=''):
     title = title.capitalize().split()
     minor_words = minor_words.lower().split()
     return ' '.join([word if word in minor_words else word.capitalize() for word in title])
+
+
+
+# Write a function that takes an array of numbers (integers for the tests)
+# and a target number. It should find two different items in the array that,
+# when added together, give the target value. The indices of these items 
+# should then be returned in a tuple like so: (index1, index2).
+
+# For the purposes of this kata, some tests may have multiple answers; 
+# any valid solutions will be accepted.
+
+# The input will always be valid (numbers will be an array of length 2 or 
+# greater, and all of the items will be numbers; target will always be the 
+# sum of two different items from that array).
+
+def two_sum(numbers, target):
+    '''Takes array of nums and a target num. Returns two ints that added together 
+    equal the target num'''
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if (i != j) and (numbers[i] + numbers[j] == target):
+                return [i, j]
