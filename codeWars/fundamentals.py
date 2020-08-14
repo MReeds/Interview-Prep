4874ac29859f463773978e96abd1170e75b3b574
MReeds <mattlreeds@gmail.com>
2020-08-14 10:33:08 -0500

    find the stray number

--- codeWars/fundamentals.py
+++ codeWars/fundamentals.py
@@ -98,3 +98,12 @@ def rmv_smallest(numbers):
             position = p
 
     return ''.join([s for s in strarr[position:position+k]])
+
+# You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+# Complete the method which accepts such an array, and returns that single different number.
+
+def stray(arr):
+    for x in arr:
+        if arr.count(x) == 1:
+            return x
