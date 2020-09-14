1647dfd2fc8ee606acbd37a6ab7064d2da23953a
Matt Reeds <59578678+MReeds@users.noreply.github.com>
2020-09-13 20:03:27 -0500

    Update Definitions.py

--- codeWars/Definitions.py
+++ codeWars/Definitions.py
@@ -15,3 +15,20 @@ print(shout('Hello'))
 yell = shout  
     
 print(yell('Hello'))
+
+# Python program to illustrate functions  
+# can be passed as arguments to other functions  
+def shout(text):  
+    return text.upper()  
+    
+def whisper(text):  
+    return text.lower()  
+    
+def greet(func):  
+    # storing the function in a variable  
+    greeting = func("Hi, I am created by a function \ 
+    passed as an argument.")  
+    print(greeting)   
+    
+greet(shout)  
+greet(whisper)
