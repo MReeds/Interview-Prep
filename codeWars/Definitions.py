8b33779771c3da4c4158f718932d31be69336c80
Matt Reeds <59578678+MReeds@users.noreply.github.com>
2020-09-13 20:01:47 -0500

    Update Definitions.py

--- codeWars/Definitions.py
+++ codeWars/Definitions.py
@@ -4,3 +4,14 @@
 # are known as Higher order Functions. It is worth knowing that this higher order function 
 # is applicable for functions and methods as well that takes functions as a parameter or returns 
 # a function as a result. Python too supports the concepts of higher order functions.
+
+# Python program to illustrate functions can be treated as objects  
+def shout(text):  
+    return text.upper()  
+    
+print(shout('Hello'))  
+    
+# Assigning function to a variable 
+yell = shout  
+    
+print(yell('Hello'))
