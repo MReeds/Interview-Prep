a67f397e23fbf8c4d6dff52f35bedb41361fd833
Matt Reeds <59578678+MReeds@users.noreply.github.com>
2020-09-21 16:55:19 -0500

    Update testing.py

--- testing/testing.py
+++ testing/testing.py
@@ -1,2 +1,9 @@
 # An integration test checks that components in your application operate with each other.
 # A unit test checks a small component in your application.
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+if __name__ == "__main__":
+    test_sum()
+    print("Everything passed")
