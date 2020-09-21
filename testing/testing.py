68ad820be62576bd6a4f6ee81377f3b7988bee9b
Matt Reeds <59578678+MReeds@users.noreply.github.com>
2020-09-21 17:03:52 -0500

    Update testing.py

--- testing/testing.py
+++ testing/testing.py
@@ -20,3 +20,24 @@
 #    test_sum()
 #    test_sum_tuple()
 #    print("Everything passed")
+
+''' Convert above test to a unittest test case.
+- Import unittest from the standard library
+- Create a class called TestSum that inherits from the TestCase class
+- Convert the test functions into methods by adding self as the first argument
+- Change the assertions to use the self.assertEqual() method on the TestCase class
+- Change the command-line entry point to call unittest.main() '''
+
+# import unittest
+
+
+# class TestSum(unittest.TestCase):
+
+#    def test_sum(self):
+#        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+#    def test_sum_tuple(self):
+#        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+# if __name__ == '__main__':
+#     unittest.main()
