d597dca830952db6bcc366a3f0fb233edd41ad61
Matt Reeds <59578678+MReeds@users.noreply.github.com>
2020-09-22 12:23:37 -0500

    Update testing.py

--- testing/testing.py
+++ testing/testing.py
@@ -41,3 +41,21 @@
 
 # if __name__ == '__main__':
 #     unittest.main()
+
+''' pytest supports execution of unittest test cases. The real advantage of pytest comes
+ by writing pytest test cases. pytest test cases are a series of functions in a
+ Python file starting with the name test_.
+
+pytest has some other great features:
+
+Support for the built-in assert statement instead of using special self.assert*() methods
+Support for filtering for test cases
+Ability to rerun from the last failing test
+An ecosystem of hundreds of plugins to extend the functionality
+Writing the TestSum test case example for pytest would look like this: '''
+
+# def test_sum():
+#    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+# def test_sum_tuple():
+#    assert sum((1, 2, 2)) == 6, "Should be 6"
